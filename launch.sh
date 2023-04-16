f8cad0851b362939c64ca4dbc23620d349793fb6
Alex Piche <alex_piche_l@hotmail.com>
2023-04-16 16:18:18 +0000

    add datasets

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
-for dataset in child alarm; do
+for dataset in child alarm asia cancer insurance; do
     for prior in 0 1; do
         for epsilon in 0.01 0.05 0.1; do
             python main.py --dataset $data --tabular 1 --epsilon $epsilon --algo global_scoring --uniform-prior $prior

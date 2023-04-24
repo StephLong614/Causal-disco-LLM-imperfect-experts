901a0a25cc49c034fe150960e518e8f931097dd2
Valentina Zantedeschi <vzantedeschi@gmail.com>
2023-04-24 17:29:01 +0100

    add bayesian posterior for noisy expert making independent errors

---
+++ models/noisy_expert.py
@@ -0,0 +1,72 @@
+from itertools import combinations, product
+import numpy as np
+
+class NoisyExpert(object):
+    def __init__(self, prior, likelihoods):
+        """
+        A class for calculating the posterior probability of edge orientations (arcs) in a Markov 
+        Equivalence Class, based on the assumption that when queried for an edge the expert returns
+        an orientation that depends only on the true orientation of that edge. 
+        Only edges that are undirected in the CPDAG are considered.
+
+        Parameters
+        ----------
+        
+        prior : callable
+            Returns the prior probability of any subset of arcs
+        
+        likelihoods : numpy.array [number of edges]
+            Likelihood of each observed arc
+        """
+
+        self._prior = prior
+        self._likelihoods = likelihoods
+    
+    def _partition_function(self, obs_arcs):
+
+        ans = 0.
+
+        for true_arcs in self._prior.get_complete_orientations():
+
+            # p(obs|true)
+            ans += self.likelihood(obs_arcs, true_arcs)
+
+        # uniform prior over DAGs in MEC
+        return ans / self._prior.mec_size()
+    
+    # likelihood of a set of orientations is factorizable P(O|E)
+    def likelihood(self, obs_arcs, true_arcs):
+        # TODO: check lists contain exactly one orientation of each edge
+
+        ans = 1.        
+        for (x1, x2) in obs_arcs:
+
+            if (x1, x2) in true_arcs:
+                ans *= self._likelihoods[(x1, x2)]
+            else:
+                ans *= 1 - self._likelihoods[(x1, x2)]
+            
+        return ans
+
+    # posterior probability of subset of possible arcs
+    def posterior(self, obs_arcs, true_arcs):
+        
+        ans = 0.
+
+        # if an edge is not oriented in the decision, need to marginalize over it
+        margin_edges = list()
+        for (x1, x2) in obs_arcs:
+
+            if ((x1, x2) not in true_arcs) and ((x2, x1) not in true_arcs):
+                margin_edges.append(((x1, x2), (x2, x1)))
+        
+        if len(margin_edges) > 0:
+        
+            # Cartesian product over possible orientations of edges
+            for arcs in product(*margin_edges):
+                ans += self.likelihood(obs_arcs, list(arcs) + true_arcs) * self._prior(list(arcs) + true_arcs)
+        
+        else:
+            ans = self.likelihood(obs_arcs, true_arcs) * self._prior(true_arcs)
+
+        return ans / self._partition_function(obs_arcs)
--- models/priors.py
+++ models/priors.py
@@ -1,10 +1,6 @@
-import matplotlib.pyplot as plt
-import networkx as nx
 import numpy as np
-import random
 
-from causaldag import DAG
-from collections import defaultdict
+from itertools import chain
 
 class MECPrior(object):
     def __init__(self, cpdag):
@@ -23,50 +19,71 @@ class MECPrior(object):
         self._cpdag = cpdag
 
         # Get all orientations for undirected CPDAG edges
-        self.unoriented_edges = list(
+        self.edges = list(
             chain(*[[(x1, x2), (x2, x1)] for x1, x2 in cpdag.edges])
         )
 
         # Assign an ID to each orientation
-        self._uedge_idx = dict(
-            zip(self.unoriented_edges, range(len(self.unoriented_edges)))
+        self._edge_idx = dict(
+            zip(self.edges, range(len(self.edges)))
         )
 
         # Prior table (n_orientations x n_dags_in_mec)
         self._prior = np.zeros(
-            (len(self.unoriented_edges), len(self._cpdag.all_dags()))
+            (len(self.edges), self.mec_size())
         )
 
         # Get occurence of each edge in all DAGs of the MEC
         for i, dag in enumerate(self._cpdag.all_dags()):
-            for edge, idx in self._uedge_idx.items():
+            for edge, idx in self._edge_idx.items():
                 if edge in dag:
                     self._prior[idx, i] = 1
 
         super().__init__()
 
-    def __call__(self, edges):
+    def __call__(self, arcs):
         """
-        Compute the probability of the given set of edges in the equivalence class.
+        Compute the probability of the given set of arcs in the equivalence class.
 
         Parameters
         ----------
-        edges : list of tuples
-            A list of edges, each represented as a tuple (source, target), where source and target
+        arcs : list of tuples
+            A list of arcs, each represented as a tuple (source, target), where source and target
             are the nodes in the graph.
 
         Returns
         -------
         float
-            The probability of the given set of edges occurring in the equivalence class of DAGs,
+            The probability of the given set of arcs occurring in the equivalence class of DAGs,
             computed as the product of the occurrence of each edge in all possible DAGs, divided
             by the total number of possible DAGs in the equivalence class.
 
         """
-        edge_idx = [self._uedge_idx[e] for e in edges]
+        edge_idx = [self._edge_idx[e] for e in arcs]
         return (
-            np.vstack([self._prior[self._uedge_idx[e]] for e in edges])
+            np.vstack([self._prior[self._edge_idx[e]] for e in arcs])
             .prod(axis=0)
             .sum()
             / self._prior.shape[1]
         )
+
+    def get_complete_orientations(self):
+        """
+        All possible complete orientations of CPDAG's edges. 
+
+        Returns
+        -------
+        generator
+            each item is a list of arcs corresponding to one DAG in the MEC.
+        """
+        for dag in self._cpdag.all_dags():
+            complete_orientation = list()
+
+            for x1, x2 in dag:
+                if self._cpdag.has_edge(x1, x2):
+                    complete_orientation.append((x1, x2))
+
+            yield complete_orientation
+    
+    def mec_size(self):
+        return len(self._cpdag.all_dags())

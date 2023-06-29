949f9927eee0b6df35f2e96f256506bbf2850bf4
Valentina Zantedeschi <vzantedeschi@gmail.com>
2023-06-29 20:16:43 +0000

    broken

--- sweep.sh
+++ sweep.sh
@@ -1,28 +1,26 @@
-for d in child alarm asia cancer insurance
-do  
-    for s in 965079 79707 239916 537973 953100
+#!/bin/bash
+source /home/toolkit/.bashrc
+conda activate noisy
+cd /home/toolkit/git/Causal-disco/
+for s in 965079 79707 239916 537973 953100
+do
+    for p in mec independent
     do
-        # l=0
-        l=1
         for e in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.
-        # for e in 0.05
         do
-            for p in mec independent
+            for t in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.
             do
-                for t in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.
+                for a in "greedy_mec" "greedy_conf"
                 do
-                    for a in "greedy_mec" "greedy_conf"
-                    do
-                        python3 main.py --tabular $l --tol $t --prior $p --algo $a --dataset $d --seed $s --epsilon $e --wandb
-                    done
+                    python3 main.py --seed=$s --tol $t --prior $p --algo $a --dataset $1 --wandb --tabular --epsilon $e
                 done
+            done
 
-                for a in "greedy_bic" "global_scoring" "blind"
-                do
-                    python3 main.py --tabular $l --tol $t --prior $p --algo $a --dataset $d --seed $s --epsilon $e --wandb
-                done
+            for a in "greedy_bic" "global_scoring" "blind"
+            do
+                python3 main.py --seed=$s --tol $t --prior $p --algo $a --dataset $1 --wandb --tabular --epsilon $e
             done
         done
     done
-    python3 main.py --tol $t --prior $p --algo "PC" --dataset $d --wandb
 done
+python3 main.py --algo "PC" --dataset $1 --wandb
